f49141188107c0aee2ece4a7ae7b40ac7838b7bf
zelebwr <jonathanzelig6013@gmail.com>
2025-05-31 19:56:33 +0700

    added printString() and updated .gitignore

--- src/kernel.c
+++ src/kernel.c
@@ -8,7 +8,19 @@ int main() {
 
 void printString(char *str)
 {
-  //TODO: Implementasi fungsi untuk mencetak string
+  int i = 0; 
+  unsigned int AX; 
+  char character_to_print; 
+
+  while (str[i] != '\0') {
+    character_to_print = str[i];
+    AX = 0x0E << 8 | (unsigned char)character_to_print;
+    // 0x0E for teletype output, 
+    // "<< 8" to shift it to the high byte, 
+    // "| (unsigned char)character_to_print" to set the low byte
+    interrupt(0x10, AX, 0, 0, 0); // Call BIOS interrupt 0x10 to print character
+    i++;
+  }
 }
 
 void readString(char *buf)
